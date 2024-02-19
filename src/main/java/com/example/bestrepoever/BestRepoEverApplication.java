7e419a8ac4511ac5e39fb86db7a6d307cc187031
Lars <laha0006@stud.kea.dk>
2024-02-19 13:02:49 +0100

    init

---
+++ src/main/java/com/example/bestrepoever/BestRepoEverApplication.java
@@ -0,0 +1,13 @@
+package com.example.bestrepoever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestRepoEverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestRepoEverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bestrepoever/BestRepoEverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bestrepoever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestRepoEverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
