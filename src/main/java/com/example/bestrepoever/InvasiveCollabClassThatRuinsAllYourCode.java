7285f951c290635feb655d4ed6114c56260e9560
Dosei-desu <krispauly@hotmail.com>
2024-02-19 13:09:30 +0100

    help

---
+++ src/main/java/com/example/bestrepoever/InvasiveCollabClassThatRuinsAllYourCode.java
@@ -0,0 +1,19 @@
+package com.example.bestrepoever;
+
+public class InvasiveCollabClassThatRuinsAllYourCode {
+
+
+    int recursive(){
+        int breaker = 1;
+        for (int i = 1; i < 10000; i++) {
+            breaker = breaker * i;
+            for (int j = 1; j < 10000; j++) {
+                breaker = breaker * j;
+                for (int k = 0; k < breaker; k++) {
+                    recursive();
+                }
+            }
+        }
+        return breaker;
+    }
+}
